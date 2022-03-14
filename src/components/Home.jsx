3d467ab15bcb82718b4a891c5cfaf85ea584e19b
jbiehl88 <jcbiehl8@gmail.com>
2022-03-14 14:23:24 -0500

    monday update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,11 @@
-import React, {useState} from "react";
+import React from 'react';
 
 const Home = () => {
-  const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  const onPasswordChange = (e) => {
-      setPassword(e.target.value)
-  }
-
-  return (
-    <div className="home">
-      <form onSubmit={handleSubmit}>
-        <input
-          value={user}
-          type="text"
-          placeholder="username"
-          onChange={(e) => setUser(e.target.value)}
-        />
-        <input value={password} type="text" placeholder="password" />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            Im a home
+        </div>
+    );
 };
 
 export default Home;
